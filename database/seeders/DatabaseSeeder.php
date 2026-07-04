11bf7d2290c60bf00e3f9939f3e46ea457450009
Nenad005 <markonenadovic2005@gmial.com>
2026-07-04 02:04:17 +0200

    activation code hashing

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,8 +3,10 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use App\Models\ActivationCode;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +17,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $user1 = User::create([
+            'first_name'   => 'Luka',
+            'last_name'    => 'Nenadovic',
+            'jmbg'         => '2410001583342',
+            'phone_number' => '+381669442557',
+            'email'        => 'luka@student.etf.bg.ac.rs',
+            'pin_hash'     => null,
+            'status'       => 'pending_activation',
         ]);
+
+        ActivationCode::create([
+            'user_id'    => $user1->id,
+            'code'       => Hash::make('LUKA-2026'),
+            'expires_at' => now()->addDays(45),
+        ]);
+
+        $user2 = User::create([
+            'first_name'   => 'Marko',
+            'last_name'    => 'Nenadovic',
+            'jmbg'         => '24100053783492',
+            'phone_number' => '+381644546204',
+            'email'        => 'mn20240174@student.fon.bg.ac.rs',
+            'pin_hash'     => null,
+            'status'       => 'pending_activation',
+        ]);
+
+        ActivationCode::create([
+            'user_id'    => $user2->id,
+            'code'       => Hash::make('MARKO-2026'),
+            'expires_at' => now()->addDays(45),
+        ]);
+
+        $this->command->info('Succesfully seeded database');
     }
 }
